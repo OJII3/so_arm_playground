dda773604087251509394665a425d597ac337331
ojii3 <84656786+OJII3@users.noreply.github.com>
2026-06-21 22:20:05 +0900

    fix(teleop_ik): use float32 stick_x/stick_y fields (Vector2 unavailable in Jazzy)

--- ros2_ws/src/teleop_ik/test/test_target_msg.py
+++ ros2_ws/src/teleop_ik/test/test_target_msg.py
@@ -10,14 +10,15 @@ def test_target_pose_with_input_instantiation():
     msg = TargetPoseWithInput()
     assert hasattr(msg, "header")
     assert hasattr(msg, "pose")
-    assert hasattr(msg, "stick")
-    assert msg.stick.x == 0.0
-    assert msg.stick.y == 0.0
+    assert hasattr(msg, "stick_x")
+    assert hasattr(msg, "stick_y")
+    assert msg.stick_x == 0.0
+    assert msg.stick_y == 0.0
 
 
 def test_target_pose_with_input_set_fields():
     msg = TargetPoseWithInput()
-    msg.stick.x = 0.5
-    msg.stick.y = -0.25
-    assert msg.stick.x == 0.5
-    assert msg.stick.y == -0.25
+    msg.stick_x = 0.5
+    msg.stick_y = -0.25
+    assert msg.stick_x == 0.5
+    assert msg.stick_y == -0.25
